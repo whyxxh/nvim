c35f925662e1e3335798d4c867b5bc98e242c9f2
whyxxh <rubenbeg0908@gmail.com>
2025-04-26 15:05:48 +0400

    clean repo

--- lua/plugins/basics.lua
+++
@@ -1,9 +0,0 @@
-return {
-    {
-        "numToStr/Comment.nvim",
-        opts = {},
-    },
-
-    "pocco81/auto-save.nvim",
-    "rafamadriz/friendly-snippets",
-}
--- lua/plugins/percistance.lua
+++
@@ -1,7 +0,0 @@
-return {
-    {
-        "folke/persistence.nvim",
-        event = "BufReadPre", -- Trigger early to ensure sessions are available
-        opts = {}, -- Use default options
-    }
-}
